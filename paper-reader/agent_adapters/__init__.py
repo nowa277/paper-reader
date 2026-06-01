377a76949c32d9b402dd8a7a281b1e76f80a4646
Nowa <sesleycheung@gmail.com>
2026-06-01 17:06:23 +0800

    fix: import adapters subpackage to trigger registration on package import

--- paper-reader/agent_adapters/__init__.py
+++ paper-reader/agent_adapters/__init__.py
@@ -2,4 +2,7 @@ from .base import BaseAdapter, AdapterConfig, GenerationResult
 from .registry import Registry
 from .generator import Generator
 
-__all__ = ["BaseAdapter", "AdapterConfig", "GenerationResult", "Registry", "Generator"]
+# Import adapters subpackage to trigger registration
+from . import adapters
+
+__all__ = ["BaseAdapter", "AdapterConfig", "GenerationResult", "Registry", "Generator", "adapters"]
